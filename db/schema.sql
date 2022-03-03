63c459dd37f64f22d6603ea4314bac0186ddbd02
Irvens R Dupuy <92906651+irvensd@users.noreply.github.com>
2022-03-02 21:27:45 -0500

    worked on server code for login

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS passwordExample; -- This is an example in order to get the app working we will change
+
+CREATE DATABASE passwordExample;
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const path = require('path');
 const express = require('express');
+const routes = require('./controllers/');
+const sequelize = require('./config/connection');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 // const routes = require('./controllers');
 
 const app = express();
@@ -10,8 +15,26 @@ const sequelize = require('./config/connection');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 //app.use(routes);
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
